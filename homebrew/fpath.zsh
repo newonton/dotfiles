5e94163007c13fc9acce63ce067aa0d5b87d5ffe
Anton Chekulaev <chekulaevanton1@gmail.com>
2025-08-05 19:44:43 +0400

    fix fpath directories rights

--- homebrew/fpath.zsh
+++ homebrew/fpath.zsh
@@ -1,1 +1,9 @@
 FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+
+if find "/opt/homebrew/share" -prune \( -perm -g+w -o -perm -o+w \) -print -quit | grep -q .; then
+  chmod go-w "/opt/homebrew/share"
+fi
+
+if find "/opt/homebrew/share/zsh" \( -perm -g+w -o -perm -o+w \) -print -quit | grep -q .; then
+  chmod -R go-w "/opt/homebrew/share/zsh"
+fi
