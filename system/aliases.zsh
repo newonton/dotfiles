43854d218e2db6d51d2890ef797c3a09f56f918e
Anton Chekulaev <chekulaevanton1@gmail.com>
2025-08-05 14:18:24 +0400

    add functions and aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,6 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+alias rm="rm -r"
+alias mkdir="mkdir -p"
--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,2 @@
 # Pipe my public key to my clipboard.
-alias pubkey="more ~/.ssh/onton.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="cat ~/.ssh/onton.pub | pbcopy | echo 'Public key copied to pasteboard.'"
