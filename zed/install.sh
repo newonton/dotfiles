c9b5d25115cc878810cdff07455fc4f9b268e72a
Anton Chekulaev <chekulaevanton1@gmail.com>
2025-08-05 19:33:53 +0400

    fix zed installer

--- zed/install.sh
+++ zed/install.sh
@@ -10,6 +10,11 @@ then
     mv "$HOME/.config/zed/settings.json" "$HOME/.config/zed/settings.json.backup"
   fi
 
+  if test -L "$HOME/.config/zed/settings.json"
+  then
+    rm "$HOME/.config/zed/settings.json"
+  fi
+
   mkdir -p "$HOME/.config/zed"
   ln -s "$HOME/.zedrc" "$HOME/.config/zed/settings.json"
   sudo ln -s "/Applications/Zed.app/Contents/MacOS/cli" "/usr/local/bin/zed"
