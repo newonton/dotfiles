18b1150ff1fc8f310ccbd79f2fa878b2b58aef1a
Anton Chekulaev <chekulaevanton1@gmail.com>
2025-12-23 15:26:26 +0400

    universal homebrew installer

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,16 +9,7 @@
 if ! command -v brew >/dev/null 2>&1
 then
   echo "  Installing Homebrew for you."
-
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-  fi
-
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 exit 0
